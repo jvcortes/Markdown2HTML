37cca173f6c3027e00bf8625db02a4931ef91ef1
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-08-06 20:51:11 -0500

    docstring and pep8

--- markdown2html.py
+++ markdown2html.py
@@ -3,9 +3,15 @@
 import os
 import sys
 
+"""
+Markdown2HTML, generates an HTML file from a Markdown file.
+"""
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print(
+            "Usage: ./markdown2html.py README.md README.html",
+            file=sys.stderr
+        )
         exit(1)
     elif not os.path.exists(sys.argv[1]):
         print("Missing {}".format(sys.argv[1]), file=sys.stderr)
