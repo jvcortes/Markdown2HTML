3017eafc9b6fa6082a01d9405f77d40e32119475
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-08-07 00:22:07 -0500

    task 1
    Added support for Markdown headings.

--- markdown2html.py
+++ markdown2html.py
@@ -3,7 +3,38 @@
 Markdown2HTML, generates an HTML file from a Markdown file.
 """
 import os
+import re
 import sys
+
+def read_file(file):
+    converted = ""
+
+    with open(file) as f:
+        for line in f:
+            result = transform(line)
+            if (result):
+                converted += result + '\n'
+        f.close()
+
+    with open("{}".format(sys.argv[2]), 'w') as f:
+        f.write(converted)
+        f.close()
+
+
+def transform(line):
+    result = line
+
+    match = re.search("^(#{1,6})\W(.*)", line)
+    if match:
+        header_level = len(match.group(1))
+        result = header(match.group(2), header_level)
+
+    return result
+
+
+def header(line, header_level):
+    return "<h{}>{}</h{}>".format(header_level, line, header_level)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -16,5 +47,5 @@ if __name__ == "__main__":
         print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         exit(1)
     else:
-        print(end='')
+        read_file(sys.argv[1])
         exit(0)
