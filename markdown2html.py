e34f7a3c41d2a19dd92aadc91ae2bf28aa0fdc84
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-08-06 20:33:37 -0500

    initial commit
    Added README.md and base script for task 0.

---
+++ markdown2html.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+    elif not os.path.exists(sys.argv[1]):
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+        exit(1)
+    else:
+        print(end='')
+        exit(0)
