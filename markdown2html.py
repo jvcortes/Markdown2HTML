b2cd1303bceeaf22f13020a46307dedfcf483dd2
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-08-07 20:32:54 -0500

    feat: unordered list characters
    added support for unnested unordered list characters (hyphens)

--- markdown2html.py
+++ markdown2html.py
@@ -11,10 +11,17 @@ def read_file(file):
 
     with open(file) as f:
         for line in f:
+
+            if line == '\n':
+                converted += '\n'
+                continue
+
             result = transform(line)
-            if (result):
+            if result:
                 converted += result + '\n'
         f.close()
+
+    converted = transform_unordered_lists(converted)
 
     with open("{}".format(sys.argv[2]), 'w') as f:
         f.write(converted)
@@ -24,16 +31,53 @@ def read_file(file):
 def transform(line):
     result = line
 
-    match = re.search("^(#{1,6})\W(.*)", line)
+    match = re.search("^(\s{0,4}-\s)(.*)", line)
+    if match:
+        content = transform(match.group(2))
+        result = list_element(content)
+
+    match = re.search("^(#{1,6})\s(.*)", line)
     if match:
         header_level = len(match.group(1))
         result = header(match.group(2), header_level)
 
     return result
 
+def transform_unordered_lists(file):
+    converted = ""
+    inside_list = False
+    split = file.splitlines()
+
+    for index, line in enumerate(split):
+
+        result = line
+        match = re.search("<uli>(.*)</uli>", line)
+        if match:
+            if not inside_list:
+                inside_list = True
+                result = "<ul>\n" + result
+
+        if not match:
+            if inside_list:
+                inside_list = False
+                result = "</ul>\n" + line
+        elif index == len(split) - 1:
+            if inside_list:
+                inside_list = False
+                result = line + "\n</ul>"
+
+        converted += result + '\n'
+    converted = converted.replace('<uli>', '<li>')
+    converted = converted.replace('</uli>', '</li>')
+
+    return converted
+
 
 def header(line, header_level):
     return "<h{}>{}</h{}>".format(header_level, line, header_level)
+
+def list_element(line):
+    return "<uli>{}</uli>".format(line)
 
 
 if __name__ == "__main__":
